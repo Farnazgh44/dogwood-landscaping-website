18126fa35e10e3cb3f1b2f0fdc76aadb5afbfbd3
Faye44701 <fgholami4@my.bcit.ca>
2026-04-02 21:11:03 -0700

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ export default function HomePage() {
           
           <AnimatedSection animation="fade-up" delay={200}>
             <h1 className="font-serif text-4xl md:text-6xl lg:text-7xl font-bold mb-6 text-balance">
-              <span style={{ color: '#fffbf8' }}>Precision in the mud,</span>
+              <span style={{ color: '#fffbf8' }}>Precision in the Mud,</span>
               <span className="block" style={{ color: '#e4aeb5' }}>Perfection in the Yard</span>
             </h1>
           </AnimatedSection>
